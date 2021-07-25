c9de6dd44afaed8f85b4250893f7e38446fe0bbf
emilstei <87337554+emilstei@users.noreply.github.com>
2021-07-25 14:43:24 -0400

    Add files via upload

---
+++ simple_madlib.py
@@ -0,0 +1,9 @@
+ # Madlibs: will ask you to type different types of words.
+# Once all the words are typed, then the computer will put together a story for you.
+object=input('Name an object in this room: ')
+food=input('Name a type of food: ')
+color=input('What is your favorite color: ')
+animal=input('Name a zoo animal: ')
+
+print('The', animal, 'jumped onto the', color, object, 'and flew across the city to eat',\
+      food, 'at his favorite restaurant.')
